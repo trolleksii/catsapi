865472a7fbec4f5aad99efee2fbcd4e2e7fd384d
trolleksii <trolleksii@gmail.com>
2018-05-03 18:21:39 +0300

    urls

--- catapi/apps/api/urls.py
+++ catapi/apps/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import re_path
 
-from .views import CatAPIView
+from .views import BreedAPIView, CatAPIView, RandomCatAPIView
+
+app_name = 'api'
+
 
 urlpatterns = [
-    re_path('', CatAPIView.as_view(), name='catapi_view'),
+    re_path(r'breeds$', BreedAPIView.as_view(), name='list_create_breed'),
+    re_path(r'breeds/(?P<breed_slug>[\w\-]+)$', CatAPIView.as_view(), name='list_create_cat'),
+    re_path(r'breeds/(?P<breed_slug>[\w\-]+)/random$', RandomCatAPIView.as_view(), name='get_random_cat'),
 ]
