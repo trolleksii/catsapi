d18b3c08eb78f58f52b7914e74af4ce915301d46
trolleksii <trolleksii@gmail.com>
2018-05-03 18:21:10 +0300

    serializers

---
+++ catapi/apps/api/serializers.py
@@ -0,0 +1,55 @@
+import re
+
+from django.utils.text import slugify
+
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from catapi.settings import STATIC_URL
+
+from .models import Breed, Cat
+
+
+class BreedSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Breed
+        fields = ('name', 'slug')
+        read_only_fields = 'slug',
+
+    def validate(self, attrs):
+        name = attrs.get('name', '')
+        # check for non alphabet symbols or spaces and bring to uniform look
+        has_bad_symbols = re.search(r'[^\s\w]', name)
+        if has_bad_symbols:
+            msg = 'Name should be only of alphabet symbols and spaces'
+            raise ValidationError(msg)
+        name = ' '.join([x.capitalize() for x in name.split()])
+        # native DRF validators do not take into account trailing spaces etc
+        if Breed.objects.filter(name=name).exists():
+            msg = 'This breed is already in the db'
+            raise ValidationError(msg)
+        return {
+            'name': name,
+            'slug': slugify(name)
+        }
+
+
+class CatSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Cat
+        fields = ('image', 'breed')
+        extra_kwargs = {
+            'breed': {'write_only': True}
+        }
+
+    @property
+    def data(self):
+        return super(serializers.Serializer, self).data
+
+    def to_representation(self, obj):
+        return STATIC_URL + obj.image.name
+
+    def create(self, validated_data):
+        return Cat.objects.create(**validated_data)
