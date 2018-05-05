79c6b1a4c3e4e27719c0a3418994a4089b1dddc4
trolleksii <trolleksii@gmail.com>
2018-05-05 15:39:33 +0300

    index page and static files

---
+++ catapi/apps/ui/static/js/catalog.js
@@ -0,0 +1,34 @@
+function queryAPIforList(addr, handler) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText)
+            console.log(data)
+            if (data["status"] == "success") {
+                handler(data["message"])
+            }
+        }
+    };
+    xhttp.open("GET", addr, true);
+    xhttp.send();
+}
+
+function populateCatList(data) {
+    sel = document.getElementById("breed_selector")
+    for (elem in data) {
+        sel.options.add( new Option(data[elem]["name"], data[elem]["slug"]))
+    }
+}
+
+function showImgKeypress() {
+    sel = document.getElementById("breed_selector")
+    slug = document.getElementById("breed_selector").selectedOptions[0].value
+    queryAPIforList("/api/breeds/"+slug+"/random", displayImage)
+}
+
+function displayImage(data) {
+    holder = document.getElementById("img_holder")
+    holder.innerHTML = "<img src='" + data + "'>"
+}
+
+queryAPIforList("/api/breeds", populateCatList)
