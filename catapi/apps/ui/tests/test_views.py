ff3b261e7a1cc6320315ed5991c8d1562f26b382
trolleksii <trolleksii@gmail.com>
2018-05-06 22:44:49 +0300

    ui tests

--- catapi/apps/ui/tests/test_views.py
+++ catapi/apps/ui/tests/test_views.py
@@ -1,13 +1,23 @@
+from django.shortcuts import reverse
 from django.test import TestCase
 
 
 class TestUIViews(TestCase):
 
     def test_indexview_template(self):
-        pass
+        response = self.client.get(
+            reverse('ui:index_page'),
+        )
+        self.assertTemplateUsed(response, 'index.html')
 
     def test_breedsview_template(self):
-        pass
+        response = self.client.get(
+            reverse('ui:breeds_page'),
+        )
+        self.assertTemplateUsed(response, 'breeds.html')
 
     def test_specsview_template(self):
-        pass
+        response = self.client.get(
+            reverse('ui:specs_page'),
+        )
+        self.assertTemplateUsed(response, 'specs.html')
