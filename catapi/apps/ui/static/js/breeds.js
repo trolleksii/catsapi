dfbe1b8568b0364a8bf4220ec5cf54aed2ed2686
trolleksii <trolleksii@gmail.com>
2018-07-09 13:03:47 -0400

    reorganized js code

--- catapi/apps/ui/static/js/breeds.js
+++ catapi/apps/ui/static/js/breeds.js
@@ -1,24 +1,32 @@
-function queryAPIforList(addr, handler) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var data = JSON.parse(this.responseText)
-            if (data["status"] == "success") {
-                handler(data["message"])
-            }
+var script = (function () {
+    var queryAPIforList = function (addr, handler) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                var data = JSON.parse(this.responseText);
+                if (data["status"] === "success") {
+                    handler(data["message"]);
+                };
+            };
+        };
+        xhttp.open("GET", addr, true);
+        xhttp.send();
+    };
+
+    var populateBreedsList = function (list) {
+        var ul = document.getElementById("breeds_list_id");
+        list.forEach(function() {
+            all_url="/api/breeds/"+li["slug"];
+            random_url="/api/breeds/"+li["slug"]+"/random";
+            ul.innerHTML += "<li>"+li["name"]+': <a href="'+all_url+'">All</a>, <a href="'+random_url+'">Random</a></li>';
+        });
+    };
+
+    return {
+        init: function() {
+            queryAPIforList("/api/breeds", populateBreedsList);
         }
     };
-    xhttp.open("GET", addr, true);
-    xhttp.send();
-}
+}());
 
-function populateBreedsList(data) {
-    ul = document.getElementById("breeds_list_id")
-    for (elem in data) {
-        all_url="/api/breeds/"+data[elem]["slug"]
-        random_url="/api/breeds/"+data[elem]["slug"]+"/random"
-        ul.innerHTML += "<li>"+data[elem]["name"]+': <a href="'+all_url+'">All</a>, <a href="'+random_url+'">Random</a></li>'
-    }
-}
-
-queryAPIforList("/api/breeds", populateBreedsList)
+script.init();
--- catapi/apps/ui/static/js/catalog.js
+++ catapi/apps/ui/static/js/catalog.js
@@ -1,33 +1,36 @@
-function queryAPIforList(addr, handler) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var data = JSON.parse(this.responseText)
-            if (data["status"] == "success") {
-                handler(data["message"])
-            }
+var script = (function () {
+    var queryAPIforList = function (addr, handler) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                var data = JSON.parse(this.responseText)
+                if (data["status"] === "success") {
+                    handler(data["message"])
+                };
+            };
+        };
+        xhttp.open("GET", addr, true);
+        xhttp.send();
+    };
+    var populateCatList = function (data) {
+        var sel = document.getElementById("breed_selector")
+        data.forEach(function () {
+            sel.options.add( new Option(breed["name"], breed["slug"]));
+        });
+    };
+    var getRandomImage = function () {
+        var slug = document.getElementById("breed_selector").selectedOptions[0].value;
+        queryAPIforList("/api/breeds/" + slug + "/random", displayImage);
+    }
+    var displayImage = function (address) {
+        var imgHolder = document.getElementById("img_holder");
+        imgHolder.innerHTML = '<a href="' + address + '"><img id="img_preview" src="' + address + '"></a>';
+    }
+    return {
+        init: function() {
+            queryAPIforList("/api/breeds", populateCatList);
+            document.querySelector('#cat_img_btn').addEventListener('click', getRandomImage);
         }
     };
-    xhttp.open("GET", addr, true);
-    xhttp.send();
-}
-
-function populateCatList(data) {
-    sel = document.getElementById("breed_selector")
-    for (elem in data) {
-        sel.options.add( new Option(data[elem]["name"], data[elem]["slug"]))
-    }
-}
-
-function showImgKeypress() {
-    sel = document.getElementById("breed_selector")
-    slug = document.getElementById("breed_selector").selectedOptions[0].value
-    queryAPIforList("/api/breeds/" + slug + "/random", displayImage)
-}
-
-function displayImage(data) {
-    holder = document.getElementById("img_holder")
-    holder.innerHTML = '<a href="' + data + '"><img id="img_preview" src="' + data + '"></a>'
-}
-
-queryAPIforList("/api/breeds", populateCatList)
+}());
+script.init()
