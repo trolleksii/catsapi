62a67f08d498e1773ba664a35e2572f32f57bafc
trolleksii <trolleksii@gmail.com>
2018-08-16 00:02:57 -0400

    check status from response_code

--- catapi/apps/api/tests/test_views.py
+++ catapi/apps/api/tests/test_views.py
@@ -9,6 +9,8 @@ from django.shortcuts import reverse
 from django.test import TestCase
 from django.utils.text import slugify
 
+from rest_framework import status
+
 from apps.api.models import Breed, Cat
 
 
@@ -25,7 +27,7 @@ class BreedAPIViewTests(TestCase):
             reverse('api:list_create_breed'),
             content_type='application/json'
         )
-        self.assertEquals(response.data['status'], 'success')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
         list_from_response = response.data['message']
         list_from_qset = Breed.objects.all()
         self.assertEquals(len(list_from_response), len(list_from_qset))
@@ -38,7 +40,7 @@ class BreedAPIViewTests(TestCase):
             data=json.dumps(data),
             content_type='application/json'
         )
-        self.assertEquals(response.data['status'], 'success')
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
         response_data = response.data['message']
         self.assertEqual(slugify(data['name']), response_data['slug'])
         breeds_after = len(Breed.objects.all())
@@ -51,7 +53,7 @@ class BreedAPIViewTests(TestCase):
             data=json.dumps(data),
             content_type='application/json'
         )
-        self.assertEquals(response.data['status'], 'error')
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
 class CatTests(TestCase):
@@ -79,7 +81,7 @@ class CatAPIViewTests(CatTests):
             content_type='application/json'
         )
         response_cats_count = len(response.data['message'])
-        self.assertEquals(response.data['status'], 'success')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
         self.assertEqual(cats_count, response_cats_count)
 
     def test_post_new_cat(self):
@@ -92,7 +94,7 @@ class CatAPIViewTests(CatTests):
             reverse('api:list_create_cat', kwargs={'breed_slug': self.breed.slug}),
             data={'files': upload_file}
         )
-        self.assertEquals(response.data['status'], 'success')
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
         final_cats_count = len(Cat.objects.filter(breed__slug=self.breed.slug))
         self.assertEquals(final_cats_count, cats_count + 1)
 
@@ -109,7 +111,7 @@ class CatAPIViewTests(CatTests):
             reverse('api:list_create_cat', kwargs={'breed_slug': self.breed.slug}),
             data={'files': [upload_file1, upload_file2]}
         )
-        self.assertEquals(response.data['status'], 'success')
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
         final_cats_count = len(Cat.objects.filter(breed__slug=self.breed.slug))
         self.assertEquals(final_cats_count, cats_count + imgs_num)
 
@@ -122,7 +124,7 @@ class CatAPIViewTests(CatTests):
             reverse('api:list_create_cat', kwargs={'breed_slug': self.breed.slug}),
             data={'files': upload_file}
         )
-        self.assertEquals(response.data['status'], 'error')
+        self.assertEquals(response.status_code, status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
 
 
 class RandomCatAPIViewTests(CatTests):
@@ -133,11 +135,23 @@ class RandomCatAPIViewTests(CatTests):
             content_type='application/json'
         )
         self.assertIn(self.breed.slug, response.data['message'])
-        self.assertEquals(response.data['status'], 'success')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
 
     def test_get_random_wrong_breed(self):
         response = self.client.get(
             reverse('api:get_random_cat', kwargs={'breed_slug': 'wrong-breed'}),
             content_type='application/json'
         )
-        self.assertEquals(response.data['status'], 'error')
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class AllCatsAPIView(CatTests):
+
+    def test_list_all_cats(self):
+        cats_number = Cat.objects.count()
+        response = self.client.get(
+            reverse('api:list_all_cats'),
+            content_type='application/json'
+        )
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(cats_number, len(response.data['message']))
