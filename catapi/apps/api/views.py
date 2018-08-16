6c72713e6cb57b61c4d59e04620a0ed801552867
trolleksii <trolleksii@gmail.com>
2018-08-16 00:01:12 -0400

    removed all redundant stuff

--- catapi/apps/api/views.py
+++ catapi/apps/api/views.py
@@ -1,6 +1,7 @@
 from random import randint
 
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 
 from rest_framework import status
 from rest_framework.permissions import AllowAny
@@ -9,9 +10,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .exceptions import PayloadTooLarge
-from .models import Breed
+from .models import Cat, Breed
 from .serializers import BreedSerializer, CatSerializer
-from .shortcuts import get_object_or_404
 
 
 class BreedAPIView(APIView):
@@ -21,25 +21,13 @@ class BreedAPIView(APIView):
     def get(self, request):
         qset = Breed.objects.all()
         serializer = self.serializer_class(qset, many=True)
-        return Response(
-            {
-                'status': 'success',
-                'message': serializer.data
-            },
-            status=status.HTTP_200_OK
-        )
+        return Response({'message': serializer.data}, status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(
-            {
-                'status': 'success',
-                'message': serializer.data
-            },
-            status=status.HTTP_201_CREATED
-        )
+        return Response({'message': serializer.data}, status=status.HTTP_201_CREATED)
 
 
 class CatAPIView(APIView):
@@ -53,13 +41,7 @@ class CatAPIView(APIView):
         breed = get_object_or_404(Breed, slug=breed_slug)
         qset = breed.cats.all()
         serializer = self.serializer_class(qset, many=True)
-        return Response(
-            {
-                'status': 'success',
-                'message': serializer.data
-            },
-            status=status.HTTP_200_OK
-        )
+        return Response({'message': serializer.data}, status=status.HTTP_200_OK)
 
     def post(self, request, breed_slug):
         breed = get_object_or_404(Breed, slug=breed_slug)
@@ -75,13 +57,7 @@ class CatAPIView(APIView):
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(
-            {
-                'status': 'success',
-                'message': serializer.data
-            },
-            status=status.HTTP_201_CREATED
-        )
+        return Response({'message': serializer.data}, status=status.HTTP_201_CREATED)
 
     def _file_size_is_ok(self, img_file):
         if img_file.size >= self.MAX_FILE_SIZE:
@@ -97,13 +73,20 @@ class RandomCatAPIView(APIView):
     def get(self, request, breed_slug):
         breed = get_object_or_404(Breed, slug=breed_slug)
         cats_count = breed.cats.aggregate(count=Count('pk'))['count']
-        data = {
-            'status': 'success',
-            'message': None
-        }
+        data = {'message': None}
         if cats_count:
             pos = randint(0, cats_count - 1)
             random_cat = breed.cats.filter()[pos]
             serializer = self.serializer_class(random_cat)
             data['message'] = serializer.data
         return Response(data, status=status.HTTP_200_OK)
+
+
+class AllCatsAPIView(APIView):
+    permission_classes = AllowAny,
+    serializer_class = CatSerializer
+
+    def get(self, request):
+        qset = Cat.objects.all()
+        serializer = self.serializer_class(qset, many=True)
+        return Response({'message': serializer.data}, status=status.HTTP_200_OK)
