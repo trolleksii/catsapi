c8021a35edcbd0f77c1dab394603ce30481dc055
trolleksii <trolleksii@gmail.com>
2018-08-16 00:00:53 -0400

    new endpoint for retrieval of all cat images

--- catapi/apps/api/urls.py
+++ catapi/apps/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import re_path
 
-from .views import BreedAPIView, CatAPIView, RandomCatAPIView
+from .views import AllCatsAPIView, BreedAPIView, CatAPIView, RandomCatAPIView
 
 app_name = 'api'
 
 
 urlpatterns = [
-    re_path(r'breeds$', BreedAPIView.as_view(), name='list_create_breed'),
-    re_path(r'breeds/(?P<breed_slug>[\w\-]+)$', CatAPIView.as_view(), name='list_create_cat'),
-    re_path(r'breeds/(?P<breed_slug>[\w\-]+)/random$', RandomCatAPIView.as_view(), name='get_random_cat'),
+    re_path(r'^$', AllCatsAPIView.as_view(), name='list_all_cats'),
+    re_path(r'^breeds$', BreedAPIView.as_view(), name='list_create_breed'),
+    re_path(r'^breeds/(?P<breed_slug>[\w\-]+)$', CatAPIView.as_view(), name='list_create_cat'),
+    re_path(r'^breeds/(?P<breed_slug>[\w\-]+)/random$', RandomCatAPIView.as_view(), name='get_random_cat'),
 ]
